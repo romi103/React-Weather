e83832dca4d473bd4d29a10b8edc65d181c2f1e1
Roman <romaan.lorent@gmail.com>
2017-02-18 15:12:10 +0000

    Example page change

--- app/component/Example.jsx
+++ app/component/Example.jsx
@@ -3,7 +3,7 @@ var React = require('React');
 var Example = (props) => {
     return (
         <div>
-            <h2>Examples Compoent</h2>
+            <h2>Examples</h2>
             <p>Welcome to examples page!</p>
         </div>
     );
