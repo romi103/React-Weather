109cb312e1948f82ff3dc6efde77c1b79de7d102
Roman <romaan.lorent@gmail.com>
2017-02-25 16:23:28 +0000

    addd search bar functionality

--- app/component/Weather.jsx
+++ app/component/Weather.jsx
@@ -20,7 +20,9 @@ var Weather = React.createClass({
 
         this.setState({
             isLoading: true,
-            errorMessage: undefined
+            errorMessage: undefined,
+            location: undefined,
+            temp: undefined
         });
 
         openWeatherMap.getTemp(location).then(function(temp){
@@ -39,6 +41,24 @@ var Weather = React.createClass({
         });
 
 
+    },
+
+    componentDidMount: function() {
+        var location = this.props.location.query.location;
+
+        if (location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash ="#/";
+        }
+    },
+
+    componentWillReceiveProps: function(newProps) {
+        var location = newProps.location.query.location;
+
+        if (location && location.length > 0) {
+            this.handleSearch(location);
+            window.location.hash = "#/";
+        }
     },
 
     render: function() {
